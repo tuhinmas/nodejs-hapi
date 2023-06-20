b0547870aa3b8076ded420633597d044e32e9f57
mastuhin <mastuhin33@gmail.com>
2023-06-20 20:23:31 +0700

    user module

--- src/server.js
+++ src/server.js
@@ -2,13 +2,19 @@
 /* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
+const users = require('./api/users');
+
 const NotesService = require('./services/postgres/NotesService');
+const UsersService = require('./services/postgres/UsersService');
 const NotesValidator = require('./validator/notes');
+const UsersValidator = require('./validator/users');
+
 require('dotenv').config();
 
 // eslint-disable-next-line no-unused-vars
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -23,13 +29,22 @@ const init = async () => {
   /**
    * plugin registration
    */
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);

1fc244c586b5f4594a7942e4a74e7005dc4421fa
mastuhin <mastuhin33@gmail.com>
2023-07-19 22:41:38 +0700

    cache using redis

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
-const path = require('path');
 const Inert = require('@hapi/inert');
 const notes = require('./api/notes');
 const users = require('./api/users');
@@ -33,18 +32,22 @@ const ExportsValidator = require('./validator/exports');
 
 // uploads
 const uploads = require('./api/uploads');
-const StorageService = require('./services/storage/StorageService');
+const StorageService = require('./services/s3/StorageService');
 const UploadsValidator = require('./validator/uploads');
+
+// cache
+const CacheService = require('./services/redis/CacheService');
 
 require('dotenv').config();
 
 // eslint-disable-next-line no-unused-vars
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
+  const cacheService = new CacheService();
+  const collaborationsService = new CollaborationsService(cacheService);
+  const notesService = new NotesService(collaborationsService, cacheService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
+  const storageService = new StorageService();
 
   const server = Hapi.server({
     port: process.env.PORT,
--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -7,8 +7,9 @@ const {
 const InvariantError = require('../../exceptions/InvariantError');
 
 class CollaborationsService {
-    constructor() {
+    constructor(cacheService) {
         this._pool = new Pool();
+        this._cacheService = cacheService;
     }
 
     async addCollaboration(noteId, userId) {
@@ -21,6 +22,8 @@ class CollaborationsService {
         if (!result.rows.length) {
             throw new InvariantError('Kolaborasi gagal ditambahkan');
         }
+
+        await this._cacheService.delete(`notes:${userId}`);
         return result.rows[0].id;
     }
 
@@ -34,6 +37,7 @@ class CollaborationsService {
         if (!result.rows.length) {
             throw new InvariantError('Kolaborasi gagal dihapus');
         }
+        await this._cacheService.delete(`notes:${userId}`);
     }
 
     async verifyCollaborator(noteId, userId) {
